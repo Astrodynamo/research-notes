5eaf17e2b552d0b546b9d08585643fff395fe491
Amanda King <amapickell@gmail.com>
2021-03-24 14:12:05 -0400

    Created Nav and stubbed out nav routes

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,3 +1,6 @@
 class TopicsController < ApplicationController
     ###create new, index, update, destroy (with all notes)
+    get '/topics' do
+        "Create or Select a Topic to view Notes"
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,21 @@ class UsersController < ApplicationController
     ###login, logout, signup, destroy acct
 
     get '/signup' do
-        if !logged_in?
-            erb :"/users/new"
-        else
-            redirect "/"
-        end
+        "Validate & create user, redirect"
+        #if !logged_in?
+        #    erb :"/users/new"
+        #else
+        #    redirect "/"
+        #end
+    end
+
+    get '/login' do
+        "Set user id in session if found, redirect"
+    end
+
+    get '/logout' do
+        session.clear if session[:user_id].true
+        redirect "/login"
     end
     
 end
